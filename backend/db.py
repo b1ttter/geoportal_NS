c0fcb16e9bc842fcf5168278b1e20575ca9d8c66
b1ttter <szumaknikita@gmail.com>
2026-03-04 19:12:12 +0100

    changed db, added docker file for postgre, changed docker-compose

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
 import psycopg2
 import os
 
+load_dotenv()
+
 def get_connection():
     return psycopg2.connect(
-        host="postgis",
+        host=os.getenv("DB_HOST"),
         port=5432,
-        database="gis",
-        user="gisuser",
-        password="gispass"
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS")
     )
